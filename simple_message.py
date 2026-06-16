167e84ec7e469047c98a2ed5789029966e6e4142
Najmul Islam <itzmenajmul@gmail.com>
2026-06-16 23:59:08 +0600

    completed 2-1 and 2-2 from try it yourself

---
+++ simple_message.py
@@ -0,0 +1,6 @@
+message = "Be positive, whatever you want to do."
+print(message);
+
+message_two = "Always be humble who knows when you would die.";
+message_two = "And death is the bruthle truth!";
+print(message_two);
--- strng.py
+++ strng.py
@@ -4,5 +4,12 @@
 
 first_name = "johir";
 last_name = "rayhan";
+result = "passed";
 
-print((first_name + " " + last_name).title());
+
+# print((first_name + " " + last_name).title());
+# print((f"Hello, {first_name} {last_name}. \n\tHe is {result} in HSC.").title())
+
+
+profession = "                  dark            ";
+print(profession.strip());
